df92931870604a446edd532ee36a7d8850723d4e
Christian Quilagüy Díaz <cquiladiaz@gmail.com>
2022-05-03 20:42:45 -0300

    avance,progreso en el header yla seccion  home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,34 @@
-/*===== MENU SHOW Y HIDDEN =====*/
+/*==== MENU SHOW ====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId);
+    nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show');
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+/* ==== ACTIVE AND REMOVE MENU ====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    //Active Link
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    //Remove menu mobile
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+/*==================== REMOVE MENU MOBILE ====================*/
 
 
-// SHOW
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-// HIDDEN
+/*===== SCROLL REVEAL ANIMATION =====*/
 
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
