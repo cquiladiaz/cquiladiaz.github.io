26b40b8a65ebeb6ab56fbfd7e5e5cd310ef29f46
Christian Quilagüy Díaz <cquiladiaz@gmail.com>
2022-06-21 22:17:51 -0300

    correción en el navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ function linkAction(){
     navMenu.classList.remove('show');
 }
 
+navLink.forEach(n => n.addEventListener('click', linkAction));
 /*==================== REMOVE MENU MOBILE ====================*/
 
 
